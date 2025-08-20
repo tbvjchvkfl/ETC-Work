1f7e39217443de4fb5c2e0f00e8658b0c71fa88d
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-08-20 20:05:18 +0900

    레벨 인스턴스 추가

--- River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Private/Level/LS_Ocean.cpp
+++ River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Private/Level/LS_Ocean.cpp
@@ -1,1 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
 
+
+// Framework
+#include "Level/LS_Ocean.h"
+#include "Character/Controller/PlayerCharacterController.h"
+#include "Character/ObjectBase.h"
+
+// Engine
+
+ALS_Ocean::ALS_Ocean()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ALS_Ocean::BeginPlay()
+{
+	Super::BeginPlay();
+	PCController = Cast<APlayerCharacterController>(GetWorld()->GetFirstPlayerController());
+}
+
+void ALS_Ocean::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	//MouseInteraction();
+}
+
+void ALS_Ocean::MouseInteraction()
+{
+	if (PCController)
+	{
+		FVector WorldLocation, WorldDirection;
+		PCController->DeprojectMousePositionToWorld(WorldLocation, WorldDirection);
+
+		FHitResult Hit;
+		FVector StartPoint = WorldLocation;
+		FVector EndPoint = StartPoint + (WorldDirection * 1000.0f);
+		FCollisionQueryParams CollisionParams;
+		CollisionParams.AddIgnoredActor(this);
+		if (GetWorld()->LineTraceSingleByChannel(Hit, StartPoint, EndPoint, ECollisionChannel::ECC_Visibility, CollisionParams))
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, FString::Printf(TEXT("Interaction : %s"), *Hit.GetActor()->GetName()));
+			if (AObjectBase* InteractionObject = Cast<AObjectBase>(Hit.GetActor()))
+			{
+				InteractionObject->ObjectInteraction();
+			}
+		}
+	}
+}
