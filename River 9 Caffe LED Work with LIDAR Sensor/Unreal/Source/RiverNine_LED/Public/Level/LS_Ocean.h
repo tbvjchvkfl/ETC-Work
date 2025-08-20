6bdcfb5cb8dac031690fc73b64b1ea9ee09a9207
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-08-20 20:04:30 +0900

    레벨 인스턴스 추가

--- River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Public/Level/LS_Ocean.h
+++ River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Public/Level/LS_Ocean.h
@@ -1,1 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
 
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LevelScriptActor.h"
+#include "LS_Ocean.generated.h"
+
+class APlayerCharacterController;
+
+UCLASS()
+class RIVERNINE_LED_API ALS_Ocean : public ALevelScriptActor
+{
+	GENERATED_BODY()
+	
+public:
+	//==============================================================
+	//=                          Variable                          =
+	//==============================================================
+
+
+	//==============================================================
+	//=                          Function                          =
+	//==============================================================
+	ALS_Ocean();
+
+private:
+	//==============================================================
+	//=                          Variable                          =
+	//==============================================================
+	APlayerCharacterController* PCController;
+
+
+	//==============================================================
+	//=                          Function                          =
+	//==============================================================
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+	void MouseInteraction();
+};
