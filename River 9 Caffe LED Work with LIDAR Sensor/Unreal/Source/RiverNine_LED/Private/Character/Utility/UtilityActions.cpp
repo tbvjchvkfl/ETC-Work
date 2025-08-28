a081ee235c24e46ba044dcd002351d02e52e5cdc
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-08-28 20:42:35 +0900

    액션 베이스 로직 수정

--- River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Private/Character/Utility/UtilityActions.cpp
+++ River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Private/Character/Utility/UtilityActions.cpp
@@ -10,6 +10,7 @@
 // Engine
 #include "GameFramework/FloatingPawnMovement.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // MoveAction
 
@@ -119,7 +120,79 @@ bool UUtilityFleeAction::CompleteAction()
 
 float UUtilityFleeAction::CalculateActionScore()
 {
+	BaseScore = 0.0f;
 	if (Owner->bIsInteraction)
+	{
+		BaseScore = 1.0f;
+		if (Owner->InteractionCount != 0 && Owner->InteractionCount % 3 == 0)
+		{
+			BaseScore = 0.0f;
+		}
+	}
+	return BaseScore;
+}
+
+void UUtilityFleeAction::ExecuteAction()
+{
+	if (TargetLocation != FVector::ZeroVector)
+	{
+		FVector MoveDirection = (TargetLocation - Owner->GetActorLocation()).GetSafeNormal();
+		MoveComp->MaxSpeed = 1200.0f;
+		MoveComp->AddInputVector(MoveDirection * Owner->FleeSpeed);
+
+		if (!MoveDirection.IsNearlyZero())
+		{
+			FRotator MoveRotation = MoveDirection.Rotation();
+			MoveRotation.Roll = Owner->GetActorRotation().Roll;
+			Owner->SetActorRotation(FMath::RInterpTo(Owner->GetActorRotation(), MoveRotation, GetWorld()->GetDeltaSeconds(), 2.0f));
+		}
+	}
+}
+
+
+
+// Hide Action
+
+void UUtilityHideAction::InitHideAction()
+{
+	if (UUtilityComponent* UtilityManager = Cast<UUtilityComponent>(GetOuter()))
+	{
+		Owner = Cast<AAnimalBase>(UtilityManager->GetOwner());
+		if (Owner)
+		{
+			MoveComp = Owner->GetFloatingMovementComponent();
+		}
+	}
+	TargetLocation = FVector::ZeroVector;
+	ActionName = FName(TEXT("Hide Action"));
+	BaseScore = 0.0f;
+}
+
+void UUtilityHideAction::SetTargetLocation()
+{
+	if (Owner)
+	{
+		TargetLocation = Owner->SetHideLocation();
+	}
+	else
+	{
+		TargetLocation = FVector::ZeroVector;
+	}
+}
+
+bool UUtilityHideAction::CompleteAction()
+{
+	if (FVector::Dist(Owner->GetActorLocation(), TargetLocation) <= 100.0f)
+	{
+		Owner->InteractionCount++;
+		return true;
+	}
+	return false;
+}
+
+float UUtilityHideAction::CalculateActionScore()
+{
+	if (Owner->InteractionCount != 0 && Owner->InteractionCount % 3 == 0)
 	{
 		BaseScore = 1.0f;
 	}
@@ -130,9 +203,8 @@ float UUtilityFleeAction::CalculateActionScore()
 	return BaseScore;
 }
 
-void UUtilityFleeAction::ExecuteAction()
+void UUtilityHideAction::ExecuteAction()
 {
-	GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, TEXT("Flee Action"));
 	if (TargetLocation != FVector::ZeroVector)
 	{
 		FVector MoveDirection = (TargetLocation - Owner->GetActorLocation()).GetSafeNormal();
