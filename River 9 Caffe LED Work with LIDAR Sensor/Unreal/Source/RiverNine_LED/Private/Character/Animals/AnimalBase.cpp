ea067fa2d7ce9ae16ec356ebe6a8fcb25fa39868
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-08-28 20:40:28 +0900

    신규 액션 추가
    - 기본 이동
    - 마우스 인터렉션 시 도망
    - 인터렉션 횟 수에 따라 말미잘로 숨음

--- River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Private/Character/Animals/AnimalBase.cpp
+++ River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Private/Character/Animals/AnimalBase.cpp
@@ -5,11 +5,13 @@
 #include "Character/Animals/AnimalBase.h"
 #include "Component/UtilityComponent.h"
 #include "Character/Utility/UtilityActionBase.h"
+#include "LevelObject/HideObject.h"
 
 // Engine
 #include "Components/SphereComponent.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "GameFramework/FloatingPawnMovement.h"
+#include "Kismet/GameplayStatics.h"
 
 
 AAnimalBase::AAnimalBase() : bIsInteraction(false), MoveSpeed(1.0f), FleeSpeed(10.0f)
@@ -31,6 +33,7 @@ void AAnimalBase::TriggerFleeAction()
 {
 	if (UtilityManager)
 	{
+		InteractionCount++;
 		for (UUtilityActionBase* Action : UtilityManager->AvailableActions)
 		{
 			if (Action->ActionName == FName(TEXT("Flee Action")))
@@ -41,6 +44,25 @@ void AAnimalBase::TriggerFleeAction()
 			}
 		}
 	}
+}
+
+FVector AAnimalBase::SetHideLocation()
+{
+	TArray<AActor*> Actors;
+	UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName("Hide Object"), Actors);
+	float MaxDistance = FLT_MAX;
+	AActor* SelectedActor = nullptr;
+	for (auto Actor : Actors)
+	{
+		float TargetDistance = FVector::Dist(GetActorLocation(), Actor->GetActorLocation());
+		if (MaxDistance > TargetDistance)
+		{
+			MaxDistance = TargetDistance;
+			SelectedActor = Actor;
+		}
+	}
+	AHideObject* HideObj = Cast<AHideObject>(SelectedActor);
+	return HideObj->GetActorLocation();
 }
 
 void AAnimalBase::BeginPlay()
