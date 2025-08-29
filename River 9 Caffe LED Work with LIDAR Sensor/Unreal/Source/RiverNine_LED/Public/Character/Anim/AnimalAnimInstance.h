4645808a966ea2ede66cf6da986ecf7ed5a5bd13
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-08-29 20:59:30 +0900

    Bone IK를 이용한 애니메이션 적용

--- River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Public/Character/Anim/AnimalAnimInstance.h
+++ River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Public/Character/Anim/AnimalAnimInstance.h
@@ -6,12 +6,55 @@
 #include "Animation/AnimInstance.h"
 #include "AnimalAnimInstance.generated.h"
 
-/**
- * 
- */
+class AAnimalBase;
+
 UCLASS()
 class RIVERNINE_LED_API UAnimalAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+	//==============================================================
+	//=                          Variable                          =
+	//==============================================================
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Swim")
+	FVector Velocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Swim")
+	float SwimFrequency = 6.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Swim")
+	float SwimAmplitude = 15.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Essential Data")
+	AAnimalBase* OwnerCharacter;
+
+	// Procedural / IK
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "IK")
+	FVector RootLocation;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "IK")
+	FVector HeadEffectorLocation;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "IK")
+	FRotator TailRotationOffset;
+
+	//==============================================================
+	//=                          Function                          =
+	//==============================================================
+	
+
+private:
+	//==============================================================
+	//=                          Variable                          =
+	//==============================================================
+	float ElapsedTime = 0.0f;
+
+	//==============================================================
+	//=                          Function                          =
+	//==============================================================
+	virtual void NativeInitializeAnimation()override;
+	virtual void NativeBeginPlay() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
 };
