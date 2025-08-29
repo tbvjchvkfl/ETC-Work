83a0f74b81b75eda54b189cc780a54e250dc2c1f
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-08-29 21:00:07 +0900

    본 IK를 이용한 애니메이션 적용

--- River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Private/Character/Anim/AnimalAnimInstance.cpp
+++ River 9 Caffe LED Work with LIDAR Sensor/Unreal/Source/RiverNine_LED/Private/Character/Anim/AnimalAnimInstance.cpp
@@ -1,5 +1,50 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+// Framework
 #include "Character/Anim/AnimalAnimInstance.h"
+#include "Character/Animals/AnimalBase.h"
 
+// Engine
+#include "Components/SkeletalMeshComponent.h"
+
+void UAnimalAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+}
+
+void UAnimalAnimInstance::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+	OwnerCharacter = Cast<AAnimalBase>(TryGetPawnOwner());
+}
+
+void UAnimalAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	ElapsedTime += DeltaSeconds;
+
+	if (!OwnerCharacter)
+	{
+		OwnerCharacter = Cast<AAnimalBase>(TryGetPawnOwner());
+	}
+	if (OwnerCharacter)
+	{
+        RootLocation = OwnerCharacter->GetActorLocation();
+        Velocity = OwnerCharacter->GetVelocity();
+
+        FVector MoveDir = Velocity.GetSafeNormal();
+        if (!MoveDir.IsNearlyZero())
+        {
+            HeadEffectorLocation = OwnerCharacter->GetActorLocation() + MoveDir * 50.f;
+        }
+
+        float Sway = FMath::Sin(ElapsedTime * SwimFrequency) * SwimAmplitude;
+        TailRotationOffset = FRotator(0.f, Sway, 0.f);
+	}
+}
+
+void UAnimalAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+}
